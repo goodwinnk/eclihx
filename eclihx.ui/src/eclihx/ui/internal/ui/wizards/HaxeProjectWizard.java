931c3045bea2509732bd30b8367ba88312103b7a
goodwinnk <goodwinnk@localhost>
2007-12-30 00:06:46 +0000

    Default project creation.

--- eclihx.ui/src/eclihx/ui/internal/ui/wizards/HaxeProjectWizard.java
+++ eclihx.ui/src/eclihx/ui/internal/ui/wizards/HaxeProjectWizard.java
@@ -1,16 +1,11 @@
 package eclihx.ui.internal.ui.wizards;
 
-//import org.eclipse.jface.wizard.Wizard;
-
-//import eclihx.ui.wizards.NewHaxeProjectWizardPage;
-
-//import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
 public class HaxeProjectWizard extends BasicNewProjectResourceWizard{
-	
+
 	@Override
 	public boolean performFinish() {
-		return true;
+		return super.performFinish();
 	}
 }
