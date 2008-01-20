734daed9a9d93623bc361ba28964225a9641e3e1
goodwinnk <goodwinnk@localhost>
2008-01-20 20:30:36 +0000

    Logger. Hope it will be usefull

---
+++ eclihx.core/src/eclihx/core/EclihxLogger.java
@@ -0,0 +1,38 @@
+package eclihx.core;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+/**
+ * Eclihx Logger Class
+ */
+
+public class EclihxLogger {
+
+	public static void logInfo(String message) {
+		log(IStatus.INFO, IStatus.OK, message, null);
+	}
+
+	public static void logError(Throwable exception) {
+		logError("Unexpected Exception", exception);
+	}
+
+	public static void logError(String message, Throwable exception) {
+		log(IStatus.ERROR, IStatus.OK, message, exception);
+	}
+
+	public static void log(int severity, int code, String message,
+			Throwable exception) {
+		log(createStatus(severity, code, message, exception));
+	}
+
+	public static IStatus createStatus(int severity, int code, String message,
+			Throwable exception) {
+		return new Status(severity, "EclihaX Plug-in", code, message,
+				exception);
+	}
+
+	public static void log(IStatus status) {
+		EclihxCore.getDefault().getLog().log(status);
+	}
+}
