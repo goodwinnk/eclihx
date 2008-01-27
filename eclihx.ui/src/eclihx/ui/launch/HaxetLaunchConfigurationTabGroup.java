2a075a8554bb7a6e2a694de9f840867471868a06
goodwinnk <goodwinnk@localhost>
2008-01-27 22:58:06 +0000

    Simple launcher

--- eclihx.ui/src/eclihx/ui/launch/HaxetLaunchConfigurationTabGroup.java
+++ eclihx.ui/src/eclihx/ui/launch/HaxetLaunchConfigurationTabGroup.java
@@ -5,6 +5,8 @@ import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 
+import eclihx.ui.launch.tabs.HaxeMainTab;
+
 public class HaxetLaunchConfigurationTabGroup extends
 		AbstractLaunchConfigurationTabGroup {
 
@@ -14,6 +16,7 @@ public class HaxetLaunchConfigurationTabGroup extends
      */
     public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
         ILaunchConfigurationTab[] tabs = {
+        		new HaxeMainTab(),
                 new CommonTab()
             };
         
