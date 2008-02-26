c45f665f13f0e60947a659bcff48c9b0796808b6
goodwinnk <goodwinnk@localhost>
2008-02-26 10:10:18 +0000

    Improve launcher

--- eclihx.launching/src/eclihx/launching/HaxeLaunchDelegate.java
+++ eclihx.launching/src/eclihx/launching/HaxeLaunchDelegate.java
@@ -1,23 +1,28 @@
 package eclihx.launching;
 
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
 
-import org.eclipse.core.runtime.Path;
 import java.io.File;
-import java.io.IOException;
 
+import eclihx.core.EclihxCore;
 import eclihx.core.EclihxLogger;
+import eclihx.core.haxe.model.core.IHaxeProject;
+
 
 public class HaxeLaunchDelegate implements ILaunchConfigurationDelegate{
 
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
 		try {
+			
             String executablePath = configuration.getAttribute(IHaxeLaunchConfigurationConstants.HAXE_COMPILER_PATH, "");
             
             if (executablePath == null || executablePath.isEmpty()) {
@@ -28,20 +33,19 @@ public class HaxeLaunchDelegate implements ILaunchConfigurationDelegate{
             if (executablePath == null || executablePath.isEmpty()) {
             	// TODO 4 Move this message to configuration page
             	EclihxLogger.logInfo("You should choose haXe launcher first.");
+            	return;
             }
-            
-            
             
             // TODO 8 get attributes
             String arguments = "";
                 
             // output directory
-            String directoryPath = configuration.getAttribute(IHaxeLaunchConfigurationConstants.OUTPUT_DIRECTORY, (String) null);
-            File directory = (directoryPath != null) ? new File(directoryPath) : null;
+            String outputPath = configuration.getAttribute(IHaxeLaunchConfigurationConstants.OUTPUT_DIRECTORY, (String) null);
+            File directory = (outputPath != null) ? new File(outputPath) : null;
             
             // source directory
             String sourceDirectory = configuration.getAttribute(IHaxeLaunchConfigurationConstants.WORKING_DIRECTORY, (String) null);
-            
+                        
             // TODO 5 dirty hack
             if (sourceDirectory.endsWith("\\")) {
             	sourceDirectory = sourceDirectory.substring(0, sourceDirectory.length() - 1);
@@ -50,32 +54,35 @@ public class HaxeLaunchDelegate implements ILaunchConfigurationDelegate{
             // hxml file
             String buildFilePath = configuration.getAttribute(IHaxeLaunchConfigurationConstants.BUILD_FILE, (String) null);
             
-            // build the command line
-            IPath executableLocation = new Path(executablePath);
-            String commandLine = executableLocation.toString() + ' ' + arguments + ' ' + "-cp \"" + sourceDirectory + "\" "  + buildFilePath;
+            String commandLine = executablePath + ' ' + arguments + ' ' + "-cp " + quoteString(sourceDirectory) + " "  + buildFilePath;
             
-            
-            EclihxLogger.logInfo(commandLine);
-            
-            Process systemProcess = Runtime.getRuntime().exec(commandLine, null, directory);
+            Process systemProcess = DebugPlugin.exec(DebugPlugin.parseArguments(commandLine), directory);
             
             DebugPlugin.newProcess(launch, systemProcess, null);
             
             // TODO 8 update bin folder
-            // IFolder executeFolder = ResourcesPlugin.getWorkspace().getRoot().getFolder(executableLocation);
-            // executeFolder.refreshLocal(2, new NullProgressMonitor());
-            
-            
+            String projectName = configuration.getAttribute(IHaxeLaunchConfigurationConstants.PROJECT_NAME, (String) null);
+            IHaxeProject haxeProject = EclihxCore.getDefault().getHaxeWorkspace().getHaxeProject(projectName);            
+            IFolder outputFolder = haxeProject.getPathManager().getOutputFolder();
+            outputFolder.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
             
         } catch (CoreException e) {
             EclihxLogger.logError(e);
-        } catch (IOException e) {
-            // stop all
-            monitor.done();
         }
-        
-        
-		
+        finally {
+            monitor.done();        	
+        }
 	}
 	
+	/**
+	 * Checks if string isn't quoted yet and adds quotation marks to the both ends of the string.
+	 * @param str
+	 * @return Quoted string
+	 */
+	private String quoteString(String str) {
+		if ( !(str.startsWith("\"") || (str.endsWith("\""))) ) {
+			return "\"" + str + "\"";
+		}
+		return str;				
+	}
 }
