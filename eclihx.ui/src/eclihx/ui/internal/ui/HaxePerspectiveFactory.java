6961758c52d2118e3ce6558920c4f9012457bafa
goodwinnk <goodwinnk@localhost>
2008-03-02 23:37:53 +0000

    New package view and warning in creating launch configuration

--- eclihx.ui/src/eclihx/ui/internal/ui/HaxePerspectiveFactory.java
+++ eclihx.ui/src/eclihx/ui/internal/ui/HaxePerspectiveFactory.java
@@ -2,29 +2,34 @@ package eclihx.ui.internal.ui;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-
 import org.eclipse.ui.IFolderLayout;
-//import org.eclipse.ui.IPageLayout;
-//import org.eclipse.ui.IPerspectiveFactory;
+
+import eclihx.ui.internal.ui.views.PackageExplorerView;
+
+
+
 
 public class HaxePerspectiveFactory implements IPerspectiveFactory {
 
+	
+	
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 
 		// Top left
 		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, (float) 0.25, editorArea);
-		// topLeft.addView(ICoreConstants.HAXE_EXPLORER_VIEW);
-		topLeft.addView(IPageLayout.ID_RES_NAV);
+		topLeft.addView(PackageExplorerView.PACKAGE_EXPLORER_ID);
 		topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
 
 		// Bottom left
-		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, (float) 0.50, "topLeft");
+		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, (float) 0.50, editorArea);
 		bottomLeft.addView(IPageLayout.ID_OUTLINE);
 
 		// Bottom right
 		IFolderLayout bottomRight = layout.createFolder("bottomRight", IPageLayout.BOTTOM, (float) 0.65, editorArea);
 		bottomRight.addView(IPageLayout.ID_TASK_LIST);
 		bottomRight.addView(IPageLayout.ID_PROBLEM_VIEW);
+		
+		
 	}
 }
---
+++ eclihx.ui/src/eclihx/ui/launch/HaxeLaunchShortcut.java
@@ -0,0 +1,18 @@
+package eclihx.ui.launch;
+
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+
+public class HaxeLaunchShortcut implements ILaunchShortcut {
+
+	@Override
+	public void launch(ISelection selection, String mode) {
+		// TODO 7 Implement this
+	}
+
+	@Override
+	public void launch(IEditorPart editor, String mode) {
+		// TODO 7 Implement this
+	}
+}
