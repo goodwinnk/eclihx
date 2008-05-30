7c621d181512c1f06f080034ec074d3c07e01321
goodwinnk <goodwinnk@localhost>
2008-05-30 00:09:16 +0000

    Update of the perspective

--- eclihx.ui/src/eclihx/ui/internal/ui/HaxePerspectiveFactory.java
+++ eclihx.ui/src/eclihx/ui/internal/ui/HaxePerspectiveFactory.java
@@ -1,5 +1,6 @@
 package eclihx.ui.internal.ui;
 
+import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import org.eclipse.ui.IFolderLayout;
@@ -11,8 +12,6 @@ import eclihx.ui.internal.ui.views.PackageExplorerView;
 
 public class HaxePerspectiveFactory implements IPerspectiveFactory {
 
-	
-	
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 
@@ -21,15 +20,17 @@ public class HaxePerspectiveFactory implements IPerspectiveFactory {
 		topLeft.addView(PackageExplorerView.PACKAGE_EXPLORER_ID);
 		topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
 
-		// Bottom left
-		//IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, (float) 0.50, editorArea);
-		//bottomLeft.addView(IPageLayout.ID_OUTLINE);
-
 		// Bottom right
 		IFolderLayout bottomRight = layout.createFolder("bottomRight", IPageLayout.BOTTOM, (float) 0.65, editorArea);
 		bottomRight.addView(IPageLayout.ID_TASK_LIST);
 		bottomRight.addView(IPageLayout.ID_PROBLEM_VIEW);
 		
+		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
 		
+		layout.addShowViewShortcut(PackageExplorerView.PACKAGE_EXPLORER_ID);
+		
+		layout.addNewWizardShortcut("eclihx.ui.internal.ui.wizards.HaxeProjectWizard"); 
+		layout.addNewWizardShortcut("eclihx.ui.internal.ui.wizards.HaxeFileWizard");
+		layout.addNewWizardShortcut("eclihx.ui.internal.ui.wizards.HaxeBuildFileWizard");
 	}
 }
