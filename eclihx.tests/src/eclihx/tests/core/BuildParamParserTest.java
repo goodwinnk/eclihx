3eaab11097efc1d75ff09cc37ba9404bd4f38a58
goodwinnk <goodwinnk@localhost>
2008-06-20 22:04:19 +0000

    - Downgrade to Java 1.5

---
+++ eclihx.tests/src/eclihx/tests/core/BuildParamParserTest.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package eclihx.tests.core;
+
+import static org.junit.Assert.*;
+
+import java.io.Console;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import eclihx.core.haxe.internal.parser.BuildParamParser;
+import eclihx.core.util.console.parser.core.ParseError;
+import eclihx.core.util.console.parser.core.Parser;
+
+/**
+ */
+public class BuildParamParserTest {
+
+	private static BuildParamParser parser;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		parser = new BuildParamParser();
+	}	
+
+	/**
+	 * Test method for {@link eclihx.core.haxe.internal.parser.BuildParamParser#parseFile(java.lang.String)}.
+	 */
+	@Test
+	public void testParseFile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link eclihx.core.haxe.internal.parser.BuildParamParser#parse(java.lang.String[])}.
+	 */
+	@Test
+	public void testParseStringArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Tests debug configuration 
+	 * Test method for {@link eclihx.core.haxe.internal.parser.BuildParamParser#parse(java.lang.String)}.
+	 */
+	@Test
+	public void debugConfigurationParse() {
+		try {
+			parser.parseString("-debug 	-D fdb   -swf Test.swf   -swf-version 9");
+		} catch (ParseError e) {
+			fail("Parse Exception: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Invalid configuration
+	 * Test method for {@link eclihx.core.haxe.internal.parser.BuildParamParser#parse(java.lang.String)}.
+	 */
+	@Test
+	public void badConfigurationParse() {
+		try {
+			parser.parseString("-debug 	-D fdb hahaha  -swf Test.swf   -swf-version 9");
+			fail("Configuration is invalid");
+		} catch (ParseError e) {
+		}
+	}
+}
