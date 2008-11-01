6697006cb7923282747f6aa5d9f3b71d34a35de2
goodwinnk <goodwinnk@localhost>
2008-11-01 16:34:54 +0000

    Make the console appears in default haXe perspective and make it auto open after launching.

--- eclihx.ui/src/eclihx/ui/internal/ui/HaxePerspectiveFactory.java
+++ eclihx.ui/src/eclihx/ui/internal/ui/HaxePerspectiveFactory.java
@@ -4,6 +4,7 @@ import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.console.IConsoleConstants;
 
 import eclihx.ui.internal.ui.views.PackageExplorerView;
 
@@ -31,11 +32,13 @@ public class HaxePerspectiveFactory implements IPerspectiveFactory {
 				"bottomRight", IPageLayout.BOTTOM, (float) 0.65, editorArea);
 		bottomRight.addView(IPageLayout.ID_TASK_LIST);
 		bottomRight.addView(IPageLayout.ID_PROBLEM_VIEW);
+		bottomRight.addView(IConsoleConstants.ID_CONSOLE_VIEW);
 		
 		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
 		
+		layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
 		layout.addShowViewShortcut(PackageExplorerView.PACKAGE_EXPLORER_ID);
-		
+				
 		layout.addNewWizardShortcut(
 				"eclihx.ui.internal.ui.wizards.HaxeProjectWizard");
 		
